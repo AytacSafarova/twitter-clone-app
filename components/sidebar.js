de3152b38888865dfbc8d4f7ff5f2f6cbc4a0ead
AytacSafarova <aytaccseferr@gmail.com>
2022-11-22 19:28:08 +0400

    next.js and tailwind configuration

--- components/sidebar.js
+++ components/sidebar.js
@@ -5,7 +5,7 @@ function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col items-start xl:items-start xl:w-[340px] p-2 fixed h-full ">
       <div className="flex items-center justify-center w-14 h-14 hoveer: Animation p-0 xl:ml-24">
-        <Image alt="img" src="https://rb.gy/ogau5a" width={30} height={30}  />
+        <Image  src="https://rb.gy/ogau5a" width={30} height={30}  />
       </div>
     </div>
   );
