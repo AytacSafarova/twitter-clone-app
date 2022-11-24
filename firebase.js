518a8066e866ac258909dd0a3b268bb892f65d14
AytacSafarova <aytaccseferr@gmail.com>
2022-11-24 20:11:29 +0400

    fix problem in image posted to firebase

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCDtfBpRp01co5IcDuk2Ji2UqGVV2C9crI",
     authDomain: "twitter-clone-e740d.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "860462775848",
     appId: "1:860462775848:web:cffa89210a98bfe9eb3f1d"
   };
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export default app;
+export { db, storage };
