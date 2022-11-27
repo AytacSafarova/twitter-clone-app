ad41ef8423d3ff41cd87765a269f48412fb58cc3
AytacSafarova <aytaccseferr@gmail.com>
2022-11-27 17:18:06 +0400

    Post detail page && see who wrote a comment

---
+++ .history/pages/[id]_20221127161434.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainP() {
+  return (
+    <div>[id]</div>
+  )
+}
+
+export default [id]
---
+++ .history/pages/[id]_20221127161441.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainPage() {
+  return (
+    <div>[id]</div>
+  )
+}
+
+export default MainPage
